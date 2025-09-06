5fbb823d88a9a9e823818e2275085e1787094070
t64979zj <jiajimmy061@gmail.com>
2025-09-06 17:49:04 +0100

    feat: set up cloud development and pass initial test

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -2,6 +2,13 @@
 App<IAppOption>({
   globalData: {},
   onLaunch() {
+    // 在 onLaunch 函数里，加入初始化代码
+    wx.cloud.init({
+      env: 'cloud1-4gzy5gp72cda52fe', // 这是你的环境ID
+      traceUser: true,
+    })
+
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
